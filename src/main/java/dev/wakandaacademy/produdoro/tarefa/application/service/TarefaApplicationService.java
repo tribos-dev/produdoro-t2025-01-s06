3325b7e4895beb5201170d5f38dcc7e918cd2426
Brian5m1th <briandiscbol@gmail.com>
2025-06-19 12:12:48 -0300

    feat: implementa fluxo do serviço, teste ok!

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationService.java
@@ -1,6 +1,7 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
 import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaDoUsuarioListResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
@@ -12,6 +13,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -39,5 +41,12 @@ public class TarefaApplicationService implements TarefaService {
         tarefa.pertenceAoUsuario(usuarioPorEmail);
         log.info("[finaliza] TarefaApplicationService - detalhaTarefa");
         return tarefa;
+    }
+
+    @Override
+    public List<TarefaDoUsuarioListResponse> buscaTarefasDoUsuario(String emailUsuario, UUID idUsuario) {
+        log.info("[start] TarefaApplicationService - buscaTarefasDoUsuario");
+        log.info("[finish] TarefaApplicationService - buscaTarefasDoUsuario");
+        return null;
     }
 }
