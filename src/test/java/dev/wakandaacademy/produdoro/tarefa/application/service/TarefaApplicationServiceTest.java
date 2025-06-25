eac62bb97b0bf613bfff9e57565a08e4b9ca0b42
Brian5m1th <briandiscbol@gmail.com>
2025-06-25 20:57:21 -0300

    test: implementa testes unitarios, deveLancarExcecaoQuandoUsuarioNaoEncontradoAoListarTarefas e deveRetornarListaTarefasDoUsuario, teste ok!

--- src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/tarefa/application/service/TarefaApplicationServiceTest.java
@@ -1,15 +1,17 @@
 package dev.wakandaacademy.produdoro.tarefa.application.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,6 +22,7 @@ import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaIdResponse;
 import dev.wakandaacademy.produdoro.tarefa.application.api.TarefaRequest;
 import dev.wakandaacademy.produdoro.tarefa.application.repository.TarefaRepository;
 import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
+import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
 class TarefaApplicationServiceTest {
@@ -31,6 +34,9 @@ class TarefaApplicationServiceTest {
     //	@MockBean
     @Mock
     TarefaRepository tarefaRepository;
+    //	@MockBean
+    @Mock
+    UsuarioRepository usuarioRepository;
 
     @Test
     void deveRetornarIdTarefaNovaCriada() {
@@ -45,9 +51,38 @@ class TarefaApplicationServiceTest {
     }
 
 
-
     public TarefaRequest getTarefaRequest() {
         TarefaRequest request = new TarefaRequest("tarefa 1", UUID.randomUUID(), null, null, 0);
         return request;
+    }
+
+    @Test
+    void deveRetornarListaTarefasDoUsuario() {
+        List<Tarefa> listTarefa = DataHelper.createListTarefa();
+        Usuario usuario = DataHelper.createUsuario();
+
+        when(usuarioRepository.buscaUsuarioPorEmail(any())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        when(tarefaRepository.buscaTarefasDoUsuario(any())).thenReturn(listTarefa);
+
+        tarefaApplicationService.buscaTarefasDoUsuario(usuario.getEmail(), usuario.getIdUsuario());
+
+        verify(usuarioRepository, times(1)).buscaUsuarioPorEmail(any());
+        verify(usuarioRepository, times(1)).buscaUsuarioPorId(any());
+        verify(tarefaRepository, times(1)).buscaTarefasDoUsuario(any());
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoUsuarioNaoEncontradoAoListarTarefas() {
+        UUID idUsuario = UUID.randomUUID();
+
+        when(usuarioRepository.buscaUsuarioPorId(idUsuario)).thenThrow(
+                APIException.build(HttpStatus.BAD_REQUEST, "Usuario não encontrado!"));
+
+
+        APIException apiException = assertThrows(APIException.class, () -> tarefaApplicationService.buscaTarefasDoUsuario("briansps@gmail.com", idUsuario));
+
+        assertEquals(HttpStatus.BAD_REQUEST, apiException.getStatusException());
+        assertEquals("Usuario não encontrado!", apiException.getMessage());
     }
 }
