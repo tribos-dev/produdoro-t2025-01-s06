fed729ec323cf99b54c3162367a4b69eb5138df0
iago <iagopsilva073@gmail.com>
2025-06-25 20:35:12 -0300

    feat: implementando camada API e controller

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioAPI.java
@@ -17,4 +17,11 @@ public interface UsuarioAPI {
 	@GetMapping(value = "/{idUsuario}")
 	@ResponseStatus(code = HttpStatus.OK)
 	UsuarioCriadoResponse buscaUsuarioPorId(@PathVariable UUID idUsuario);
+
+	@PatchMapping(value = "/pausa-longa/{idUsuaio}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	void mudaStatusParaPausaLonga(@RequestHeader(name = "Authorization",
+			required = true) String token, @PathVariable UUID idUsario);
+
+
 }
